dacce8a791d3b23f6825d779beab023d4dd4d945
Dennis Huang <chop.chop.dennis@gmail.com>
2022-02-14 17:48:34 +0800

    Update Logo.

---
+++ src/admin/app.js
@@ -0,0 +1,13 @@
+import AuthLogo from './extensions/phoenix-auth-logo.png'
+import MenuLogo from './extensions/phoenix-menu-logo.png'
+export default {
+    config: {
+        auth: {
+            logo: AuthLogo
+        },
+        menu:{
+            logo: MenuLogo
+        },
+        tutorials: false
+    }
+}
