cafeaf34df0109ae570f9bcabb61b1912ab2ac3b
Dennis Huang <chop.chop.dennis@gmail.com>
2022-02-15 13:13:57 +0800

    Set sendgrid email plugins

---
+++ config/plugins.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+    email: {
+      provider: 'sendgrid',
+      providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+      },
+      settings: {
+        defaultFrom: 'dennis.huang@phoenixgroup.asia',
+        defaultReplyTo: 'dennis.huang@phoenixgroup.asia',
+        testAddress: 'chop.chop.dennis@gmail.com',
+      },
+    },
+  });
